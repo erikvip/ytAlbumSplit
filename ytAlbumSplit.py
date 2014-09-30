70199d6656378ab2b11420ea5dd9b4f1d626f196
Erik Phillips <erikvip@gmail.com>
2014-09-30 13:48:40 -0600

    Fixed duration time bug / reverted from earlier

--- ytAlbumSplit.py
+++ ytAlbumSplit.py
@@ -31,7 +31,7 @@ from feedparser import *
 
 from subprocess import call
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', filename='run.log')
 
 def main():
 
@@ -53,7 +53,11 @@ def main():
 	# Query Youtube to find the video title
 	api = 'https://gdata.youtube.com/feeds/api/videos?q=%s&v=2' % urllib.quote_plus(video_id)
 
+	logging.info('Youtube API URL: %s' % api)
+
 	yt = feedparser.parse(api)
+
+	logging.info('Youtube API Response: \n %s' % yt)
 
 	if yt['entries'] == False:
 		print "No videos found"
@@ -61,17 +65,21 @@ def main():
 
 	title = yt['entries'][0]['title']
 
+	logging.info('Raw title: %s' % title)
+
 	# Remove anything betwen parenthesis or brackets
 	# This is to remove (FULL ALBUM) or [COMPLETE ALBUM] and similar..
 	title = re.sub("[\(\[].*[\)\]]", "", title).encode('utf-8').strip()
+
+	logging.info('Scrubbed title: %s' % title)
 
 
 	# We now have 'title' and URL attributes
 
 	# Download the youtube video using youtube-dl and convert it to an mp3
 	cmd = 'bin/youtube-dl.exe -f "best" -o "%(id)s.%(ext)s" -x --audio-format "mp3" ' + url
-	print "Beginning download via " + cmd
-	call(cmd)
+	logging.info('Youtube-dl command line: %s' % cmd)
+	#call(cmd)
 
 	# Now generate the CUE sheet
 	yta = YtAlbum()
@@ -87,6 +95,8 @@ def main():
 
 	cuesheet = cuesheet.encode('utf-8').strip()
 
+	logging.info('Cuesheet:\n %s' % cuesheet)
+
 	# Save the cue file to (video_id).cue
 	f = open(video_id + ".cue", "w")
 	f.write(cuesheet)
@@ -95,12 +105,12 @@ def main():
 	# Now run mp3splt
 	#spltcmd = 'bin/mp3splt.exe -c '+video_id+'.cue -T 2 -o "@b - @n2 - @t" ' + video_id + ".mp3"
 	splitcmd = "bin/mp3splt/mp3splt.exe -c %s.cue -T 2 -o \"@b - @n2 - @t\" -d \"%s\\%s\" %s.mp3" % (video_id, outputDir, title, video_id)
-	print "Splitting MP3 tracks via " + splitcmd
+	logging.info('mp3splt command line: %s' % splitcmd)
 	call(splitcmd)
 
 	# Cleaup
-	os.remove(video_id + ".mp3")
-	os.remove(video_id + ".cue")
+	#os.remove(video_id + ".mp3")
+	#os.remove(video_id + ".cue")
 
 
 class YtAlbum:
@@ -119,6 +129,9 @@ class YtAlbum:
 		mb.set_rate_limit(True)
 		res = mb.search_releases(query='artist:'+name,limit=limit)
 
+		logging.info('musicbrainz query: %s' % name)
+		logging.info('musicbrainz response: \n %s' % res)
+
 		for a in res['release-list']:
 			album = {
 				'id': a['id'], 
@@ -134,15 +147,16 @@ class YtAlbum:
 
 			output = "TITLE \"%(artist)s - %(title)s\"\n" % album
 			output += "PERFORMER \"%(artist)s\"\n" % album
-			output += "FILE \"%s.mp3\" MP3\n" % video_id
+			output += "FILE \"%s.mp3l\" MP3\n" % a['id']
 
 			time = 0
 			minutes = 0
 			seconds = 0
 
+			logging.info('musicbrainz release response: \n %s' % disc)
+
 			for d in disc['release']['medium-list']:
 				
-
 				for t in d['track-list']:
 					track = {
 						'position': str(t['position'].zfill(2)), 
@@ -150,7 +164,7 @@ class YtAlbum:
 						'minutes' : str(minutes).zfill(2), 
 						'seconds ': seconds,
 						'duration' : "%s:%s" % (str(minutes).zfill(2), seconds), 
-#						'seconds' : time
+						'seconds' : time
 					}
 
 					output += "  TRACK %(position)s AUDIO\n" % track
@@ -161,9 +175,9 @@ class YtAlbum:
 
 
 					# Convert recording time to Minutes:Seconds
-#					time += int(t['length']) / 1000
-#					minutes = time/60
-#					seconds = str(time%60)[:2].zfill(2)
+					time += int(t['length']) / 1000
+					minutes = time/60
+					seconds = str(time%60)[:2].zfill(2)
 					
 
 #					logging.info('Track #%(position)s \tDuration: %(duration)s [%(seconds)ss] \t%(title)s' % track)
